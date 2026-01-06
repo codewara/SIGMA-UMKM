0db3049adedf7ec1dce1a89f240a5af919138f2e
Daneswara <150428419+codewara@users.noreply.github.com>
2026-01-06 18:34:06 +0700

    fix typescript errors

--- app/api/umkm/[id]/route.ts
+++ app/api/umkm/[id]/route.ts
@@ -1,18 +1,16 @@
-import { connectMongo } from "@/lib/db";
 import { umkmUpdateSchema } from "@/lib/validation/umkm_profile.schema";
-import { UUID } from "mongodb";
+import { connectMongo } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
-
-type UmkmProfile = {
-    _id : UUID;
-}
+import { ZodError } from "zod";
+import { UUID } from "mongodb";
 
 // get profile umkm by id
 export async function GET(req: NextRequest, context: { params: Promise<{ id: string }>}) {
     const { id } = await context.params; 
     const db = await connectMongo();
+    const umkmCollection = db.collection("umkm_profiles");
 
-    const umkmCollection = db.collection<UmkmProfile>("umkm_profiles");
+    // @ts-expect-error cast _id to UUID
     const umkm = await umkmCollection.findOne({ _id: new UUID(id) });
     if (!umkm) {
         return NextResponse.json(
@@ -28,7 +26,7 @@ export async function PUT(req: NextRequest, context: { params: Promise<{ id: str
     try{
         const { id } = await context.params;
         const db = await connectMongo();
-        const umkmCollection = db.collection<UmkmProfile>("umkm_profiles");
+        const umkmCollection = db.collection("umkm_profiles");
 
         const reqBody = await req.json();
 
@@ -36,42 +34,30 @@ export async function PUT(req: NextRequest, context: { params: Promise<{ id: str
         const parsed = umkmUpdateSchema.partial().parse(reqBody);
 
         const result = await umkmCollection.findOneAndUpdate(
+            // @ts-expect-error cast _id to UUID
             { _id: new UUID(id) },
             { $set: parsed },
             { returnDocument: "after" }
         );
         return NextResponse.json(result!);
-    } catch (err: any) {
-        if (err?.issues) {
-            return NextResponse.json(
-                { error: "Validation failed", details: err.issues },
-                { status: 400 }
-            );
+    } catch (err) {
+        if (err instanceof ZodError) {
+            return NextResponse.json({ error: "Validation failed", details: err.issues }, { status: 400 });
         }
-
-        return NextResponse.json(
-            { error: "Failed to update UMKM", err},
-            { status: 500 }
-        );
+        return NextResponse.json({ error: "Failed to update UMKM", err}, { status: 500 });
     }
 }
 
 // delete profile umkm
-export async function DELETE(req: NextRequest, context: { params: Promise<{ id: string }> }){
+export async function DELETE(context: { params: Promise<{ id: string }> }){
     try {
         const { id } = await context.params;
         const db = await connectMongo();
-        const collection = db.collection<UmkmProfile>("umkm_profiles");
+        const collection = db.collection("umkm_profiles");
 
+        // @ts-expect-error cast _id to UUID
         const result = await collection.deleteOne({ _id: new UUID(id) });
-        return NextResponse.json(
-            { message: "UMKM Berhasil Dihapus", deletedCount: result.deletedCount },
-            { status: 200 }
-        );
-    } catch (err: any){
-        return NextResponse.json(
-            { error: "Failed to delete UMKM" },
-            { status: 500 }
-        );
+        return NextResponse.json({ message: "UMKM Berhasil Dihapus", deletedCount: result.deletedCount }, { status: 200 });
     }
+    catch { return NextResponse.json({ error: "Failed to delete UMKM" }, { status: 500 }); }
 }
